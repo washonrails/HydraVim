47135f32e69339fb7777d7a1524aeb2044960dea
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-10-30 09:54:42 -0300

    fix intall

--- script/install.sh
+++ script/install.sh
@@ -5,9 +5,9 @@ rm -rf ~/.local/share/nvim
 rm -rf ~/.cache/nvim
 
 cd /home/$USER/.config/
-git clone https://github.com/Diogo-ss/SUNvim.git --depth 1
+git clone https://github.com/SolarVim/Solar.git --depth 1
 
-mv /home/$USER/.config/SUNvim /home/$USER/.config/nvim
+mv /home/$USER/.config/Solar /home/$USER/.config/nvim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
