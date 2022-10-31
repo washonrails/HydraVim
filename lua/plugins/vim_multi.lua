724f697be504c2bcb2c3e6a24423757799cb7375
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-10-31 12:59:01 -0300

    fix multi cursor

--- lua/plugins/vim_multi.lua
+++ lua/plugins/vim_multi.lua
@@ -1,7 +1,7 @@
 vim.cmd([[
 let g:VM_maps = {}
-let g:VM_maps['Find Under']         = '<C-d>'           " replace C-n
-let g:VM_maps['Find Subword Under'] = '<C-d>'           " replace visual C-n
-let g:VM_maps["Add Cursor Down"]             = '<C-A-Down>'
-let g:VM_maps["Add Cursor Up"]               = '<C-A-Up>'
+let g:VM_maps['Find Under']                 = '<C-n>'
+let g:VM_maps['Find Subword Under']         = '<C-n>'
+let g:VM_maps["Add Cursor Down"]            = '<C-A-Down>'
+let g:VM_maps["Add Cursor Up"]              = '<C-A-Up>'
 ]])
