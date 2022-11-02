4a28a401611da867fe74885683059aadf66f9ea4
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-11-02 14:54:02 -0300

    add .php in autosave

--- lua/config/autosave.lua
+++ lua/config/autosave.lua
@@ -3,4 +3,5 @@ autocmd BufNewFile,BufRead *.md :autocmd TextChanged,TextChangedI <buffer> silen
 autocmd BufNewFile,BufRead *.js :autocmd TextChanged,TextChangedI <buffer> silent write
 autocmd BufNewFile,BufRead *.css :autocmd TextChanged,TextChangedI <buffer> silent write
 autocmd BufNewFile,BufRead *.html :autocmd TextChanged,TextChangedI <buffer> silent write
+autocmd BufNewFile,BufRead *.php :autocmd TextChanged,TextChangedI <buffer> silent write
 ]])
