d144c22f65f9ba405cfba8df2012517f753799d3
washonrails <iamx.3ur@protonmail.com>
2022-11-02 03:08:17 -0300

    sessions now have been added

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -11,6 +11,9 @@ map('i', '<C-Q>', '<ESC><CMD>q!<CR>')						-- força saída no mode inserir
 -- salvar alterações
 map('n', '<C-s>', '<CMD>w<CR>', {silent = true, noremap = true}) 	-- salva alerações no modo normal
 
+map('n', '<A-s>', '<CMD> :mksession! .Session.vim<CR>', {silent = true, noremap = true}) --        salva a sessao atual ex:abas,nerdtree,terminais etc..
+map('n', '<leader>ss', '<CMD> :source .Session.vim<CR>', {silent = true, noremap = true}) --       abre a ultima sessao salva.
+
 -- mover linhas
 map('n', '<A-j>', '<CMD>m .+1<CR>==')						-- move a linha atual para baixo no modo normal
 map('n', '<A-k>', '<CMD>m .-2<CR>==')						-- move a linha atual para cima no modo normal
