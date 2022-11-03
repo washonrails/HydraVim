32d33bd4b358dd66bf038c99bff9497e7ad59fc8
Diogo Silva <diogosantosdasilva6@gmail.com>
2022-11-03 15:47:33 -0300

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -17,7 +17,5 @@ git clone https://github.com/SolarVim/Solar.git --depth 1
 
 mv /home/$USER/.config/Solar /home/$USER/.config/nvim
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-nvim -c 'PlugInstall'
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
