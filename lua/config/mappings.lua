36e03a74c826701ceb916ff762c120df7593909c
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-11-03 17:54:13 -0300

    fix telescope

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -60,17 +60,6 @@ map('n', '<C-a>', 'ggVG')									-- selecionar tudo no modo normal
 -- snippets
 vc([[xmap <leader>sp  <Plug>(coc-convert-snippet)]])
 
--- telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
-vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
-vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
-vim.keymap.set('n', '<leader>gt', builtin.git_status, {})
-
 -- lazygit
 map('n', '<leader>lg', "<CMD>TermExec size=10 direction=float cmd='lazygit && exit'<CR>")
 
