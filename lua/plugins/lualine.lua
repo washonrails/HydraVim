d40b07a5206521a24b171931c1820e8f368e1c71
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-11-03 20:53:23 -0300

    rouded luline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,9 +3,9 @@ require('lualine').setup {
 	symbols = { added = ' ', modified = ' ', removed = ' ', error = ' ', warn = ' ', info = ' ', hint = ' '},
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
 --     component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
 --     section_separators = { left = '', right = ''},
 --    
 disabled_filetypes = {
