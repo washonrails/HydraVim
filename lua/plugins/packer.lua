01108e9e13573deb656d570e026dd78e5252a0c8
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-11-03 15:45:57 -0300

    add packer

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -1,53 +1,53 @@
-vim.cmd([[
-if empty(glob('~/.vim/autoload/plug.vim'))
-  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
-    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-endif
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
-autocmd VimEnter * if len(filter(values(g:plugs), '!isdirectory(v:val.dir)'))
-  \| PlugInstall --sync | source $MYVIMRC
-\| endif
+local packer_bootstrap = ensure_packer()
 
-call plug#begin('~/.vim/vim.plug')
+return require('packer').startup(function(use)
+	
+	use 'wbthomason/packer.nvim'
+	use 'SolarVim/nvim-treesitter'
+	use 'SolarVim/nvim-tree.lua'
+	use 'SolarVim/nvim-lspconfig'
+	use 'SolarVim/lualine.nvim'
+	use { 'SolarVim/coc.nvim', run = 'yarn install --frozen-lockfile' }
+	use 'SolarVim/gitsigns.nvim'
+	use 'SolarVim/vim-polyglot'
+	use 'SolarVim/bufferline.nvim'
+	use 'SolarVim/toggleterm.nvim'
+	use 'SolarVim/telescope.nvim'
+	use 'SolarVim/nvim-web-devicons'
+	use 'SolarVim/lazy-lsp.nvim'
+	use 'SolarVim/alpha-nvim'
+	use 'SolarVim/plenary.nvim'
+	use 'SolarVim/indent-blankline.nvim'
+	use 'SolarVim/vim-devicons'
+	use 'SolarVim/vim-visual-multi'
+	use 'SolarVim/nvim-colorizer.lua'
+	use 'SolarVim/vim-snippets'
 
-Plug 'SolarVim/nvim-treesitter'
-Plug 'SolarVim/nvim-tree.lua'
-Plug 'SolarVim/nvim-lspconfig'
-Plug 'SolarVim/lualine.nvim'
-Plug 'SolarVim/coc.nvim', {'do': 'yarn install --frozen-lockfile'}
-Plug 'SolarVim/gitsigns.nvim'
-Plug 'SolarVim/vim-polyglot'
-Plug 'SolarVim/bufferline.nvim'
-Plug 'SolarVim/toggleterm.nvim'
-Plug 'SolarVim/telescope.nvim'
-Plug 'SolarVim/nvim-web-devicons'
-Plug 'SolarVim/lazy-lsp.nvim'
-Plug 'SolarVim/alpha-nvim'
-Plug 'SolarVim/plenary.nvim'
-Plug 'SolarVim/indent-blankline.nvim'
-Plug 'SolarVim/vim-devicons'
-Plug 'SolarVim/vim-visual-multi'
-Plug 'SolarVim/nvim-colorizer.lua'
-Plug 'SolarVim/indent-blankline.nvim'
-Plug 'SolarVim/vim-snippets'
+	-- temas
+	use 'navarasu/onedark.nvim'
+	use 'tiagovla/tokyodark.nvim'
+	use 'folke/tokyonight.nvim'
+	use 'ellisonleao/gruvbox.nvim'
+	use 'luisiacc/gruvbox-baby'
+	use 'rafamadriz/neon'
+	use 'shaunsingh/nord.nvim'
+	use 'rmehri01/onenord.nvim'
+	use { "catppuccin/nvim", as = "catppuccin" }
+	use 'JoosepAlviste/palenightfall.nvim'
+	use 'projekt0n/github-nvim-theme'
 
-" temas --
-Plug 'navarasu/onedark.nvim'
-Plug 'tiagovla/tokyodark.nvim'
-Plug 'folke/tokyonight.nvim'
-Plug 'ellisonleao/gruvbox.nvim'
-Plug 'luisiacc/gruvbox-baby'
-Plug 'rafamadriz/neon'
-Plug 'shaunsingh/nord.nvim'
-Plug 'rmehri01/onenord.nvim'
-Plug 'catppuccin/nvim', { 'as': 'catppuccin' }
-Plug 'JoosepAlviste/palenightfall.nvim'
-Plug 'projekt0n/github-nvim-theme'
-
-" opt
-" Plug 'makerj/vim-pdf'
-" Plug 'shaunsingh/oxocarbon.nvim', { 'do': './install.sh' }
-" Plug 'andweeb/presence.nvim'
-
-call plug#end()
-]])
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
