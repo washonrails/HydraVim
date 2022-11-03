dbb814c5c96d5e7056745efaed4ca0d056a2bbed
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-11-03 14:39:53 -0300

    add icon save

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -9,7 +9,7 @@ map('v', '<C-Q>', '<ESC><CMD>q!<CR>')						-- força saída no mode visual
 map('i', '<C-Q>', '<ESC><CMD>q!<CR>')						-- força saída no mode inserir
 
 -- salvar alterações
-map('n', '<C-s>', '<CMD>w<CR>', {silent = true, noremap = true}) 	-- salva alerações no modo normal
+map('n', '<C-s>', "<CMD>w<CR><CMD>echo 'Save '<CR>", {silent = true, noremap = true}) 	-- salva alerações no modo normal
 
 map('n', '<A-s>', '<CMD> :mksession! .Session.vim<CR>', {silent = true, noremap = true}) --        salva a sessao atual ex:abas,nerdtree,terminais etc..
 map('n', '<leader>ss', '<CMD> :source .Session.vim<CR>', {silent = true, noremap = true}) --       abre a ultima sessao salva.
