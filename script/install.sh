d2b1c43bdc86ec0a569d54a8f162659b53ccbd71
Diogo Silva <diogosantosdasilva6@gmail.com>
2022-11-03 15:49:27 -0300

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -19,3 +19,5 @@ mv /home/$USER/.config/Solar /home/$USER/.config/nvim
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+nvim
