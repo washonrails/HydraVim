38109f3c3c8e71647e1d66fb08f9225f73b5bcb2
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-11-03 14:20:22 -0300

    icons in alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -39,9 +39,9 @@ dashboard.section.header.val =
 -- }
 
 dashboard.section.buttons.val = {
-    dashboard.button( "1", "  - New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "2", "  - Find file", ":Telescope find_files<CR>"),
-    dashboard.button( "3", "ﭯ  - Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "1", "  - New file" , ":ene <BAR> startinsert <CR>"),
+    dashboard.button( "2", "  - Find file", ":Telescope find_files<CR>"),
+    dashboard.button( "3", "  - Recent"   , ":Telescope oldfiles<CR>"),
     dashboard.button( "4", "  - Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k <CR>"),
     dashboard.button( "5", "  - Quit", ":qa<CR>"),
 }
@@ -51,3 +51,5 @@ alpha.setup(dashboard.opts)
 vim.cmd([[
     autocmd FileType alpha setlocal nofoldenable
 ]])
+
+
