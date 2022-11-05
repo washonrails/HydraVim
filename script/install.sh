dd8e4b4cd4302c4e4abda65f9e2fe37cdf614999
Diogo Silva <diogosantosdasilva6@gmail.com>
2022-11-05 20:00:17 -0300

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -21,4 +21,4 @@ mv /home/$USER/.config/HydraVim /home/$USER/.config/nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-nvim -c 'PlugInstall'
+nvim -c 'PlugInstall | quit | Alpha'
