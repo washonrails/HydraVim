0dfc307700e7ce050a425adf97d57891bbf04647
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-11-05 10:06:52 -0300

    add LSP message

--- lua/lsp/lsp_config.lua
+++ lua/lsp/lsp_config.lua
@@ -6,9 +6,29 @@ vim.diagnostic.config({
   underline = true,
   update_in_insert = true,
   severity_sort = false,
-  virtual_text = true,
-  virtual_text = { prefix = '■',} -- Could be '●', '▎', 'x'
+  virtual_text = false,
+--   virtual_text = { prefix = '■',} -- Could be '●', '▎', 'x'
 })
+
+function PrintDiagnostics(opts, bufnr, line_nr, client_id)
+  bufnr = bufnr or 0
+  line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
+  opts = opts or {['lnum'] = line_nr}
+
+  local line_diagnostics = vim.diagnostic.get(bufnr, opts)
+  if vim.tbl_isempty(line_diagnostics) then return end
+
+  local diagnostic_message = ""
+  for i, diagnostic in ipairs(line_diagnostics) do
+    diagnostic_message = diagnostic_message .. string.format("LSP: %s", diagnostic.message or "")
+    print(diagnostic_message)
+    if i ~= #line_diagnostics then
+      diagnostic_message = diagnostic_message .. "\n"
+    end
+  end
+  vim.api.nvim_echo({{diagnostic_message, "Normal"}}, false, {})
+end
+vim.cmd [[ autocmd! CursorHold * lua PrintDiagnostics() ]]
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
