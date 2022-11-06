cd7215f6de7c52c9a38344785ba4db98a8e0ed58
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-11-05 22:47:42 -0300

    comment suport

--- lua/config/comment.lua
+++ lua/config/comment.lua
@@ -1,13 +1,12 @@
 vim.cmd([[
 augroup commenting_blocks_of_code
 	autocmd!
-	autocmd FileType c,cpp,java,scala let b:comment_leader = '// '
-	autocmd FileType sh,ruby,python   let b:comment_leader = '# '
-	autocmd FileType conf,fstab       let b:comment_leader = '# '
-	autocmd FileType tex              let b:comment_leader = '% '
-	autocmd FileType mail             let b:comment_leader = '> '
-	autocmd FileType vim              let b:comment_leader = '" '
-	autocmd FileType lua              let b:comment_leader = '-- '
+	autocmd FileType c,cpp,java,scala,php                    let b:comment_leader = '// '
+	autocmd FileType sh,ruby,python,zsh,bash,cr,conf,fstab   let b:comment_leader = '# '
+	autocmd FileType tex                                     let b:comment_leader = '% '
+	autocmd FileType mail                                    let b:comment_leader = '> '
+	autocmd FileType vim                                     let b:comment_leader = '" '
+	autocmd FileType lua                                     let b:comment_leader = '-- '
     
 augroup END
 noremap <silent> ; :<C-B>silent <C-E>s/^/<C-R>=escape(b:comment_leader,'\/')<CR>/<CR>:nohlsearch<CR>
