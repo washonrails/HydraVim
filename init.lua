985e6265ac35d487871ebfa93312c26e75fb7fef
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-11-06 09:46:14 -0300

    clear code

--- init.lua
+++ init.lua
@@ -1,28 +1,30 @@
-require ('plugins.vim-plug')
+-- plugins
+require ('plugins.vim_plug')
 -- require ('plugins.packer')
 
 -- config
-require ('config.settings')
-require ('config.mappings')
-require ('config.comment')
 require ('config.autosave')
-
--- plugins config
-require ('plugins.nvim_tree')
-require ('plugins.telescope')
-require ('plugins.lualine')
-require ('plugins.indent_line')
-require ('plugins.buffer')
-require ('plugins.alpha')
-require ('plugins.gitsigns')
-require ('plugins.colorizer')
-require ('plugins.toggleterm')
-require ('plugins.which_key')
+require ('config.comment')
+require ('config.mappings')
+require ('config.settings')
 
 -- lsp
 require ('lsp.coc')
 require ('lsp.vim_treesitter')
 require ('lsp.lsp_config')
+
+-- plugins config
+require ('plugins.alpha')
+require ('plugins.buffer')
+require ('plugins.colorizer')
+require ('plugins.gitsigns')
+require ('plugins.indent_line')
+require ('plugins.lualine')
+require ('plugins.nvim_tree')
+require ('plugins.telescope')
+require ('plugins.toggleterm')
+require ('plugins.which_key')
+
 
 -- temas
 require ('theme.theme')
