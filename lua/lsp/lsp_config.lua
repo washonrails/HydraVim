82bae9c01454475fa750b3ce206f61a8f7326614
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-11-07 16:18:38 -0300

    add lsp  update

--- lua/lsp/lsp_config.lua
+++ lua/lsp/lsp_config.lua
@@ -1,5 +1,5 @@
 local nvim_lsp = require('lspconfig')
-local servers = {'ccls','clangd', 'pyright', 'tsserver', "html", "gopls", "jsonls", "rust_analyzer", "sumneko_lua", "vimls", "cssls", "jsonls"}
+local servers = {'ccls','clangd', 'pyright', 'tsserver', 'html', 'gopls', 'jsonls', 'rust_analyzer', 'sumneko_lua', 'vimls', 'cssls', 'jsonls', 'ruby_ls', 'elixirls', 'crystalline'}
 
 vim.diagnostic.config({
   signs = true,
