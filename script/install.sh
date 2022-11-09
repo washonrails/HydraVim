af4975f797fd4583e841f97f6dd0dd5255e82feb
Diogo Silva <diogosantosdasilva6@gmail.com>
2022-11-08 23:38:22 -0300

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -2,38 +2,26 @@
 
 clear
 
-ssh=0
-
-function logo() {
-	echo -e $"\033[33m
+echo -e $"\033[33m
 ⢰⣶  ⣶ ⢶⣆⢀⣶⠂⣶⡶⠶⣦⡄⢰⣶⠶⢶⣦  ⣴⣶
 ⢸⣿⠶⠶⣿ ⠈⢻⣿⠁ ⣿⡇ ⢸⣿⢸⣿⢶⣾⠏ ⣸⣟⣹⣧
 ⠸⠿  ⠿  ⠸⠿  ⠿⠷⠶⠿⠃⠸⠿⠄⠙⠷⠤⠿⠉⠉⠿⠆
-	\033[0m"
-}
+\033[0m"
 
-function rm_file() {
-	rm -rf ~/.config/nvim
-	rm -rf ~/.local/share/nvim
-	rm -rf ~/.cache/nvim
-	rm -rf ~/.config/HydraVim
-	rm -rf /home/$USER/.vim
-}
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+rm -rf ~/.cache/nvim
+rm -rf ~/.config/HydraVim
+rm -rf ~/.config/coc
+rm -rf /home/$USER/.vim
 
-function vim_plug() {
-	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-}
+cd /home/$USER/.config/
 
-function clone_ssh() {
-	git clone git@github.com:HydraVim/HydraVim.git ~/.config/nvim --depth 1 && nvim -c 'PlugInstall'
-}
+git clone https://github.com/HydraVim/HydraVim.git --depth 1
 
-function clone_https() {
-	git clone https://github.com/HydraVim/HydraVim.git --depth 1 && nvim -c 'PlugInstall'
-}
+mv /home/$USER/.config/HydraVim /home/$USER/.config/nvim
 
-logo
-rm_file
-vim_plug
-clone_https
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+nvim -c 'PlugInstall'
