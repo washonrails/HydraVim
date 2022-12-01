f19e95071a93b551b97cf29eb2fc29e9b2a637bc
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-12-01 15:21:10 -0300

    silent exit VM

--- lua/plugins/vim_multi.lua
+++ lua/plugins/vim_multi.lua
@@ -1,6 +1,7 @@
 vim.cmd [[
 let g:VM_maps = {}
 let g:VM_default_mappings = 0
+let g:VM_silent_exit = 1
 let g:VM_maps['Find Under']            = '<C-n>'
 let g:VM_maps['Find Subword Under']    = '<C-n>'
 let g:VM_maps["Select Cursor Down"]    = '<C-A-Down>'
