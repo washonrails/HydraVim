2dca4ffe92039efd4416140204428c545ceb3dda
Diogo-ss <ssdiogo@proton.me>
2023-01-16 01:38:18 -0300

    feat: add keybinds for navegation in isert mode

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -8,6 +8,12 @@ map('n', '<C-Q>', '<ESC><CMD>q!<CR>')						-- força saída no mode normal
 map('v', '<C-Q>', '<ESC><CMD>q!<CR>')						-- força saída no mode visual	
 map('i', '<C-Q>', '<ESC><CMD>q!<CR>')						-- força saída no mode inserir
 
+-- navegar no modo insert
+map('i', '<C-h>', '<Left>')
+map('i', '<C-j>', '<Down>')
+map('i', '<C-k>', '<Up>')
+map('i', '<C-l>', '<Right>')
+
 -- salvar alterações
 map('n', '<C-s>', "<CMD>w<CR><CMD>echo 'Save '<CR>") 	-- salva alerações no modo normal
 
