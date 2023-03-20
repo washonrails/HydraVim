6ae48270800dda100de1a69047d78d9c0575cbe2
washonrails <iamx.3ur@protonmail.com>
2023-03-20 02:24:46 -0300

    chore: add command for error handler

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -8,4 +8,8 @@ H.Map = function(mode, keys, comd, desc)
     vim.keymap.set(mode, keys, comd, desc, { silent = true, noremap = true })
 end
 
+H.GetError = function ()
+  vim.cmd "redir @+ | messages | redir END"
+end
+
 return H
