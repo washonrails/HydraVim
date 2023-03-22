9a492f147638e02f884dab2631cad718e369616d
washonrails <iamx.3ur@protonmail.com>
2023-03-22 20:34:32 -0300

    lint: move colors settings of scope globe to table options

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -29,9 +29,27 @@ local options = {
 	g = {
 		mapleader = " ",
 		maplocalleader = " ",
+
 		neovide_scale_factor = 0.8,
 		neovide_hide_mouse_when_typing = true,
 		neovide_transparency = 0.8,
+
+		terminal_color_0 = "#282828",
+		terminal_color_1 = "#ff6961",
+		terminal_color_2 = "#77dd77",
+		terminal_color_3 = "#fdfd96",
+		terminal_color_4 = "#458588",
+		terminal_color_5 = "#b16286",
+		terminal_color_6 = "#77dd77",
+		terminal_color_7 = "#a89984",
+		terminal_color_8 = "#928374",
+		terminal_color_9 = "#fb4934",
+		terminal_color_10 = "#77dd77",
+		terminal_color_11 = "#ffe180",
+		terminal_color_12 = "#83a598",
+		terminal_color_13 = "#d3869b",
+		terminal_color_14 = "#77dd77",
+		terminal_color_15 = "#ebdbb2",
 	},
 }
 
@@ -40,20 +58,3 @@ for type, table in pairs(options) do
 		vim[type][option] = value
 	end
 end
-
-g.terminal_color_0 = "#282828"
-g.terminal_color_1 = "#ff6961"
-g.terminal_color_2 = "#77dd77"
-g.terminal_color_3 = "#fdfd96"
-g.terminal_color_4 = "#458588"
-g.terminal_color_5 = "#b16286"
-g.terminal_color_6 = "#77dd77"
-g.terminal_color_7 = "#a89984"
-g.terminal_color_8 = "#928374"
-g.terminal_color_9 = "#fb4934"
-g.terminal_color_10 = "#77dd77"
-g.terminal_color_11 = "#ffe180"
-g.terminal_color_12 = "#83a598"
-g.terminal_color_13 = "#d3869b"
-g.terminal_color_14 = "#77dd77"
-g.terminal_color_15 = "#ebdbb2"
--- lua/lsp/lsp_config.lua
+++ lua/lsp/lsp_config.lua
@@ -36,18 +36,6 @@ vim.api.nvim_create_autocmd("CursorHold", {
 	end,
 })
 
--- local signs = {
--- 	Error = " ",
--- 	Warn = " ",
--- 	Hint = " ",
--- 	Info = " ",
--- }
-
--- for type, icon in pairs(signs) do
--- 	local hl = "DiagnosticSign" .. type
--- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
--- end
-
 mason.setup()
 mason_lspconfig.setup({
 	ensure_installed = { "lua_ls" },
