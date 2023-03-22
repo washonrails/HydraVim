7eecdf7cd12157d3537bef5289ea01aa0ff9a883
washonrails <iamx.3ur@protonmail.com>
2023-03-22 16:32:01 -0300

    chore: fix color correction bug

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,4 +1,5 @@
 local cmd = vim.cmd
+local g = vim.g
 
 local options = {
 	opt = {
@@ -39,3 +40,20 @@ for type, table in pairs(options) do
 		vim[type][option] = value
 	end
 end
+
+g.terminal_color_0 = "#282828"
+g.terminal_color_1 = "#ff6961"
+g.terminal_color_2 = "#77dd77"
+g.terminal_color_3 = "#fdfd96"
+g.terminal_color_4 = "#458588"
+g.terminal_color_5 = "#b16286"
+g.terminal_color_6 = "#77dd77"
+g.terminal_color_7 = "#a89984"
+g.terminal_color_8 = "#928374"
+g.terminal_color_9 = "#fb4934"
+g.terminal_color_10 = "#77dd77"
+g.terminal_color_11 = "#ffe180"
+g.terminal_color_12 = "#83a598"
+g.terminal_color_13 = "#d3869b"
+g.terminal_color_14 = "#77dd77"
+g.terminal_color_15 = "#ebdbb2"
