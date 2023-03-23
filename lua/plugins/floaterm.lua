a10e9381c8ea4d1ad13d2ecd22f06db3149ebc05
washonrails <iamx.3ur@protonmail.com>
2023-03-23 00:59:40 -0300

    nothing nothing nothing

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -2,8 +2,24 @@
 vim.g.floaterm_position = "center"
 vim.g.floaterm_width = 0.8
 vim.g.floaterm_height = 0.8
-vim.g.floaterm_borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"}
+vim.g.floaterm_borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
 vim.g.floaterm_autoclose = 2
 vim.g.floaterm_opener = "edit"
 vim.g.floaterm_title = ""
 vim.g.floaterm_wintitle = 0
+
+local H = {}
+
+H.which_key = function()
+	-- Configurações do which key
+	vim.g.floaterm_position = "bottom"
+	vim.g.floaterm_width = 0.8
+	vim.g.floaterm_height = 0.2
+	vim.g.floaterm_borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+	vim.g.floaterm_autoclose = 2
+	vim.g.floaterm_opener = "edit"
+	vim.g.floaterm_title = ""
+	vim.g.floaterm_wintitle = 0
+end
+
+return H
