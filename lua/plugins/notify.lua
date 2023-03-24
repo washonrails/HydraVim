5fb8d45048f5dca97ac544c0157b1a17631c328a
washonrails <iamx.3ur@protonmail.com>
2023-03-24 17:06:02 -0300

    refactor: roll back notify config

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,86 +1,86 @@
-notify = require("notify")
-notify.setup({
-	timer_interval = 10000,
-	stages = "slide",
-	render = "minimal",
-	timeout = 2000,
-})
+local function delayed_load()
+    local notify = require('notify')
+    local path = require('plenary.path')
 
-local uv = vim.loop
-local HOME = uv.os_homedir()
-local token_path = HOME .. "/.github_token"
-local token = ""
+    -- Configuração padrão
+    notify.setup({
+        timer_interval = 10000,
+        stages = "slide",
+        render = "minimal",
+        timeout = 2000,
+        background_colour = "#FFFFFF",
+        icons = {
+            ERROR = "",
+            WARN = "",
+            INFO = "",
+            DEBUG = "",
+            TRACE = "✎",
+        },
+    })
 
--- Lê o token do arquivo, se existir
-local function read_token()
-	local f = io.open(token_path, "r")
-	if f ~= nil then
-		token = f:read("*all")
-		f:close()
-	end
+    -- Armazena o número de seguidores atual
+    local followers_count = nil
+
+    -- Armazena o tempo em que a última notificação foi exibida
+    local last_notification_time = 0
+
+    -- Função que verifica se o usuário tem notificações ou mudanças em seus seguidores no Github
+    local function check_notifications(token)
+        -- Verifica o número de notificações
+        local cmd = "curl -s -H 'Authorization: token " .. token .. "' https://api.github.com/notifications"
+        local handle = io.popen(cmd)
+        local result = handle:read("*a")
+        handle:close()
+
+        local json = vim.fn.json_decode(result)
+        local notifications_count = #json
+
+        -- Verifica o número de seguidores
+        cmd = "curl -s -H 'Authorization: token " .. token .. "' https://api.github.com/user"
+        handle = io.popen(cmd)
+        result = handle:read("*a")
+        handle:close()
+
+        json = vim.fn.json_decode(result)
+        local current_followers_count = json.followers
+
+        -- Verifica se houve mudança no número de seguidores
+        if followers_count ~= nil and current_followers_count ~= followers_count then
+            local message = "Você agora tem " .. current_followers_count .. " seguidores no GitHub!"
+            notify(message, "info")
+            last_notification_time = vim.loop.now()
+        end
+
+        -- Atualiza o número de seguidores
+        followers_count = current_followers_count
+
+        -- Exibe a notificação com a quantidade de notificações, se a última notificação foi exibida há mais de 5 minutos
+        if notifications_count > 0 and (vim.loop.now() - last_notification_time) > 300000 then
+            notify(notifications_count .. " notificações no Github", "info")
+            last_notification_time = vim.loop.now()
+        end
+    end
+
+    -- Lê o token do arquivo, se existir
+    local token_path = path:new(vim.fn.expand("$HOME") .. "/.github_token")
+    local token = token_path:exists() and token_path:read() or ""
+
+    -- Se o token não estiver vazio, executa a função check_notifications
+    if token ~= "" then
+        check_notifications(token)
+    end
+
+    -- Pede a entrada do usuário para a token e executa a função check_notifications
+    local function run_check_notifications()
+        if token == "" then
+            token = vim.fn.input("Insira sua token do GitHub: ")
+            token_path:write(token, "w")
+        end
+        check_notifications(token)
+    end
+
+    -- Executa a verificação a cada 1 hora
+    vim.loop.new_timer():start(0, 300000, vim.schedule_wrap(run_check_notifications))
 end
 
--- Escreve o token no arquivo
-local function write_token()
-	local f = io.open(token_path, "w")
-	f:write(token)
-	f:close()
-end
-
--- Verifica o número de notificações
-local function check_notifications()
-	local cmd = "curl -s -H 'Authorization: token " .. token .. "' https://api.github.com/notifications"
-	local handle = io.popen(cmd)
-	local result = handle:read("*a")
-	handle:close()
-
-	local json = vim.fn.json_decode(result)
-	local current_notifications_count = json.notifications
-	local notifications_count = #json
-
-	if notifications_count == 0 or current_notifications_count == notifications_count then
-		return
-	end
-
-	notify(notifications_count .. " notificações no Github", "info")
-	check_followers()
-end
-
--- Verifica o número de seguidores
-local followers_count = nil
-function check_followers()
-	local cmd = "curl -s -H 'Authorization: token " .. token .. "' https://api.github.com/user"
-	local handle = io.popen(cmd)
-	local result = handle:read("*a")
-	handle:close()
-
-	json = vim.fn.json_decode(result)
-	local current_followers_count = json.followers
-
-	-- Verifica se houve mudança no número de seguidores
-	if followers_count ~= nil and current_followers_count ~= followers_count then
-		local message = "Você agora tem " .. current_followers_count .. " seguidores no GitHub!"
-		notify(message, "info")
-	end
-
-	-- Atualiza o número de seguidores
-	followers_count = current_followers_count
-end
-
--- Verifica o token e executa as verificações
-local function token_handler()
-	if token == "" then
-		token = vim.fn.input("Insira sua token do GitHub: ")
-		write_token()
-	end
-
-	check_notifications()
-end
-
--- Inicializa o timer
-local function init()
-	read_token()
-	uv.new_timer():start(0, 60 * 60 * 1000, vim.schedule_wrap(token_handler))
-end
-
-init()
+vim.defer_fn(delayed_load, 5000)
