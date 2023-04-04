1e3550c6c9d0007b5a49426f029b16ee4df12b01
washonrails <iamx.3ur@protonmail.com>
2023-04-04 14:32:31 -0300

    fix: recicle.lua was buggin colorscheme theme

--- lua/config/command.lua
+++ lua/config/command.lua
@@ -1,14 +1,12 @@
 local utils = require("core.utils")
 local updater = require("core.updater")
-local recicle = require("core.recicle")
 
 local commands = {
-    { "ReloadVim", utils.ReloadVimrc, {}},
-    { "UpdateSync", updater.sync_config_repo, {}},
-    { "Errors", utils.GetError , {}},
-	{ "Runner", recicle.runnerWindow, {}}
+	{ "ReloadVim",  utils.ReloadVimrc,        {} },
+	{ "UpdateSync", updater.sync_config_repo, {} },
+	{ "Errors",     utils.GetError,           {} },
 }
 
 for _, command in ipairs(commands) do
-    vim.api.nvim_create_user_command(command[1], command[2], command[3])
+	vim.api.nvim_create_user_command(command[1], command[2], command[3])
 end
