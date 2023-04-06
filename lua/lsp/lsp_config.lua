ed4ab7519fb6ff48e6ee089f8c6a32f3bdbc8643
washonrails <iamx.3ur@protonmail.com>
2023-04-06 02:27:22 -0300

    refactor/feat: add icons of mason installer and will be removed some lines soon

--- lua/lsp/lsp_config.lua
+++ lua/lsp/lsp_config.lua
@@ -1,7 +1,7 @@
-local present_l, mason = pcall(require, "mason")
-local present_m, mason_lspconfig = pcall(require, "mason-lspconfig")
-local present, lspconfig = pcall(require, "lspconfig")
-local border = require("core.border").rouded
+local present_l, mason = pcall(require, 'mason')
+local present_m, mason_lspconfig = pcall(require, 'mason-lspconfig')
+local present, lspconfig = pcall(require, 'lspconfig')
+local border = require('core.border').rouded
 
 if not (present and present_l and present_m) then
 	return
@@ -14,38 +14,49 @@ vim.diagnostic.config({
 	severity_sort = false,
 	virtual_text = false,
 	float = {
-		header = "",
-		border = border("rouded"),
+		header = '',
+		border = border('rouded'),
 		format = function(diagnostic)
-			return string.format("%s", diagnostic.message)
+			return string.format('%s', diagnostic.message)
 		end,
 	},
 })
 
-vim.api.nvim_create_autocmd("CursorHold", {
+vim.api.nvim_create_autocmd('CursorHold', {
 	buffer = bufnr,
 	callback = function()
 		local opts = {
 			focusable = false,
-			close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-			source = "always",
-			prefix = "",
-			scope = "cursor",
+			close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
+			source = 'always',
+			prefix = '',
+			scope = 'cursor',
 		}
 		vim.diagnostic.open_float(nil, opts)
 	end,
 })
 
-mason.setup()
+mason.setup({
+    ui = {
+        icons = {
+            package_pending = ' ',
+            package_installed = ' ',
+            package_uninstalled = ' ',
+        },
+    },
+})
 mason_lspconfig.setup({
-	ensure_installed = { "lua_ls" },
+	ensure_installed = { 'lua_ls' },
 	automatic_installation = true,
 })
 
 local lsp_defaults = lspconfig.util.default_config
 
-lsp_defaults.capabilities =
-	vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
+lsp_defaults.capabilities = vim.tbl_deep_extend(
+	'force',
+	lsp_defaults.capabilities,
+	require('cmp_nvim_lsp').default_capabilities()
+)
 
 mason_lspconfig.setup_handlers({
 	function(server)
